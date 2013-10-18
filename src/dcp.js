733bad990536ef856e8c29a385e8874cb444b789
skxu <skx@berkeley.edu>
2013-10-18 15:13:17 -0700

    Testing cross domain JSON-P request

--- src/dcp.js
+++ src/dcp.js
@@ -1,32 +1,35 @@
-var options;
-var breakfast;
-var lunch;
-var dinner;
-var example =[];
+
+
+var DCP = (function() {
+    var DEBUG = true;
+    var options;
+    var breakfast;
+    var lunch;
+    var dinner;
+    var URI = 'http://ocf.berkeley.edu/~eye/cal-dining/menu?date=2013-10-18&meal=lunch&location=foothill'
+    var example =[];
+
+    return {
+        parseData: function(data) {
+            console.log(DEBUG);
+            console.log(data);
+            breakfast = data.contents.breakfast;
+            for (location in breakfast) {
+                example.push(location);
+                if (DEBUG) console.log(example);
+            }
+            $('#main').append('hello world');
+        },
+        URI: function() {
+            return URI;
+        }
+    };
+})();
+
+
 
 $(document).ready(function() {
-	$.getJSON('http://www.anyorigin.com/get?url=ocf.berkeley.edu/~eye/cal-dining/menu?date=2013-10-18&meal=lunch&location=foothill&callback=parseResponse()', function(data) {
-		breakfast = data.contents.breakfast;
+    $.getJSON('http://whateverorigin.org/get?url=' + encodeURIComponent(DCP.URI()) + '&callback=?', DCP.parseData);
 
-		//jsonp = data;
-		console.log(data);
-	});
-
-
-	$.ajax({
-		url: 'http://www.anyorigin.com/get?url=ocf.berkeley.edu/~eye/cal-dining/menu?date=2013-10-18&meal=lunch&location=foothill',
-		dataType: 'jsonp',
-		jsonpCallback: 'parseResponse',
-	});
-	
 });
 
-parseResponse = function(data) {
-	breakfast = data.contents.breakfast;
-	for (food in breakfast) {
-		console.log(food);
-		example.push(food);
-	}
-	$('#main').append('hello');
-	console.log(example);
-}
